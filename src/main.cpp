748e2f7e444b3f0c2b03795bffc684c73abaa172
Tauno Erik <sedumacre@gmail.com>
2021-03-23 21:47:59 +0200

    added pixels

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
  * Projeckt  Media Dial
  * Board     Arduino Pro Micro 5v/16Mhz
  * Hardware  Rotary encoder, Pixel Ring 12 x RGB LED
- * Edited    21.03.2021
+ * Edited    23.03.2021
  * Copyright 2021 Tauno Erik
  * https://taunoerik.art/
  * 
@@ -17,10 +17,19 @@
 // https://github.com/arduino-libraries/Mouse
 // https://www.arduino.cc/reference/en/language/functions/usb/mouse/
 
-//#define DEBUG
+#define DEBUG
 
 #include "tauno_debug.h"
 #include "tauno_rotary_encoder.h"
+#include <Adafruit_NeoPixel.h>
+
+// LEDs pin
+const uint8_t LEDS_PIN = 8;
+const uint8_t LEDS_NUM = 12;
+
+const uint8_t loop_interval = 500;
+
+Adafruit_NeoPixel pixels(LEDS_NUM, LEDS_PIN, NEO_GRB + NEO_KHZ800);
 
 // Rotary Encoder pins
 const int RE_SW_PIN  = 2;
@@ -29,14 +38,43 @@ const int RE_DT_PIN  = 6;
 
 Tauno_Rotary_Encoder rotary_encoder(RE_SW_PIN, RE_CLK_PIN, RE_DT_PIN);
 
+/*
+ *
+ */
+void pixels_routine(uint8_t delay) {
+  static uint32_t _prev_millis = 0;
+  static uint8_t _i = 0;
+
+  if ((millis() - _prev_millis) >= delay) {
+    pixels.clear();
+
+    if (_i < LEDS_NUM) {
+      DEBUG_PRINT(" prev_millis=");
+      DEBUG_PRINT(_prev_millis);
+      pixels.setPixelColor(_i, pixels.Color(150, 0, 0));
+      pixels.show();
+      _prev_millis = millis();
+      _i++;
+    }
+
+    if (_i == LEDS_NUM) {
+      _i = 0;
+    }
+  }
+}
+
 
 void setup() {
   Serial.begin(9600);
 
   rotary_encoder.begin();
+
+  pixels.begin();
 }
 
 void loop() {
+  pixels_routine(loop_interval);
+
   int r = rotary_encoder.read();
   uint16_t s = rotary_encoder.speed();
 
